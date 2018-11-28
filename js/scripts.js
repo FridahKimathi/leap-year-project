4b1348c6556125a86c515d7d404976abb3d4d0bb
fridah kim <fredakim18@gmail.com>
2018-11-28 12:40:16 +0300

    add css styles used

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
-var leap=parseInt(prompt("Enter the year"));
-if (leap  %4 ==0 && leap %100==0)  {
-     if (leap % 400==0){
-          console.log("It's a leap year");
-     }else{
-          console.log ("It's not a leap year");
-     }
+
+function myFunction(){
+  var leap=document.getElementById("year").value;
+  var result;
+  if (leap  %4 ==0 && leap %100==0)  {
+    if (leap % 400==0){
+         result="It's a leap year";
+    }else{
+         result="It's not a leap year";
+    }
 } else if(leap %4==0 ){
-     console.log("it's a leap year");
+       result="it's a leap year";
 } else {
-     console.log("it's not a leap year");
+    result="it's not a leap year";
+}
+    alert(result);
 }
